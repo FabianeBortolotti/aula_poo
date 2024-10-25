a58bea9e0071b4393ef3e980301df6b0ab6788d5
FabianeBortolotti <fabiane.cad@gmail.com>
2024-10-25 17:13:31 -0300

    Adicionar arquivos de projeto.

---
+++ aula_poo/ClasseExemplo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+double calcularArea(double b, double h)
+{
+    return b * h;
+}
+
+double calcularPerimetro(double b,double h) {
+    return (b + h) * 2;
+}
+
+namespace aula_poo
+{
+    internal class ClasseExemplo
+    {
+        calcularArea(b,h);
+        calcularPerimetro();
+    }
+}
---
+++ aula_poo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_poo
+{
+
+    internal class Program {
+
+        static void Main(string[] args) { 
+            
+            
+        }
+    
+    }
+
+}
